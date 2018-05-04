e26ec0aa1431fd2010a7cbda2240a141172a9c74
pqrs <alvaro@iMac-de-Alvaro.local>
2018-05-04 10:27:10 +0200

    Minor changes

--- tests/test.php
+++ tests/test.php
@@ -5,9 +5,9 @@ require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Compos
 use TelegramMSG\Message;
 
 // Your telegram User ID
-define( "TELEGRAM_USER_ID", "XXXXXXXX"                                      );
+define( "TELEGRAM_USER_ID", "XXXXXXXX" );
 
 // Telegram bot token
 define( "BOT_AUTH_TOKEN",   "XXXXXXXXX:XXXXXXXXXXXXXXXXXXXXXXXXXXX-XXXXXXX" );
 
-echo Message::send( TELEGRAM_USER_ID, BOT_AUTH_TOKEN, "This a test." );
+echo Message::send( TELEGRAM_USER_ID, BOT_AUTH_TOKEN, "Hello world!" );
